447e72b06cd3bcb50dd1ba546490bd9d17410cdf
iboffa <i.boffa@hotmail.it>
2023-01-09 11:56:08 +0200

    Initial commit

---
+++ electron/main.ts
@@ -0,0 +1,45 @@
+import * as path from 'path';
+
+import { app, BrowserWindow } from 'electron';
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, './preload.js'),
+    },
+  });
+
+  // Load the index.html of the app.
+  win.loadFile(path.join(__dirname, '../app/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('web-contents-created', (event, webContents) => {
+  // Set the Content-Security-Policy for all web contents
+  const csp =
+    "default-src 'self'; script-src 'self' 'nonce-{random-string}'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self';";
+  webContents.session.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Content-Security-Policy': [csp],
+      },
+    });
+  });
+});
